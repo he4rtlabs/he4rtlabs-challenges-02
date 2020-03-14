4659f1ffeeba7181857134d9c17a6e053ed04a7e
Daniel He4rt <idanielreiss@gmail.com>
2020-03-14 02:26:47 -0300

    alterações e arquivos base

---
+++ src/assets/js/main.js
@@ -0,0 +1,13 @@
+// Este será o objeto principal no qual você irá se basear para as funções do desafio!
+// Caso haja dúvidas de como prosseguir, favor consultar a sala #js no nosso discord!
+let features = [
+  {
+    feature: "Authentication",
+    devHours: 10,
+    testHours: 2
+  }
+];
+
+alert("He4rtDevs <3");
+
+// Dica: faça o layout e depois pense em como vai funcionar o script.
